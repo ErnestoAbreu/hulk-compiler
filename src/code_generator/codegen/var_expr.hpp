61ffdd90bebfe3dec7aa3289463a2f42d1ab9a4a
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-06-15 23:25:09 -0400

    fix var_expr

--- src/code_generator/codegen/var_expr.hpp
+++ src/code_generator/codegen/var_expr.hpp
@@ -1,42 +1,32 @@
 #ifndef HULK_CODEGEN_VAR_EXPR_HPP
 #define HULK_CODEGEN_VAR_EXPR_HPP 1
 
+#include "../../ast/ast"
 #include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Function.h"  
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DerivedTypes.h"
-
-#include "../../ast/ast"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
 
 namespace hulk {
-    namespace ast {
+    
+namespace ast {
 
-        llvm::Value* var_expr::codegen() {
-            if (object) {
-                llvm::Value* object_value = (*object)->codegen();
+llvm::Value* var_expr::codegen() {
+  auto it = NamedValues.find(name.get_lexeme());
+  if (it == NamedValues.end()) return nullptr;
 
-                llvm::PointerType* object_type = llvm::cast<llvm::PointerType>(object_value->getType());
-                llvm::StructType* st = llvm::cast<llvm::StructType>(object_type->getNonOpaquePointerElementType());
-                
-                std::string struct_name = st->getName().str();
-                auto struct_it = StructFieldIndices.find(struct_name);
+  llvm::Value* alloca = it->second;
 
-                auto field_map = struct_it->second;
-                auto field_it = field_map.find(name.get_lexeme());
+  llvm::AllocaInst* allocaInst = llvm::dyn_cast<llvm::AllocaInst>(alloca);
+  if (!allocaInst) return nullptr;
 
-                auto index = field_it->second;
-                llvm::Value* gep = Builder->CreateStructGEP(st, object_value, index, "fieldptr");
-                return Builder->CreateLoad(st->getElementType(index), gep, name.get_lexeme());
-            }
+  llvm::Type* valueType = allocaInst->getAllocatedType();
+  return Builder->CreateLoad(valueType, alloca, name.get_lexeme());
+}
 
-            auto it = NamedValues.find(name.get_lexeme());
-            llvm::Value* alloca = it->second;
-            llvm::Type* type = alloca->getType()->getNonOpaquePointerElementType();
-            return Builder->CreateLoad(type, alloca, name.get_lexeme());
-        }
+}  // namespace ast
 
-    } // namespace ast
-} // namespace hulk
+}  // namespace hulk
 
-#endif // HULK_CODEGEN_VAR_EXPR_HPP
+#endif  // HULK_CODEGEN_VAR_EXPR_HPP

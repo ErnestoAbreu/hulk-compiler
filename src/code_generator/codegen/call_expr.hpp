10f7de3ddab2ccf7c335d7c40bec95671b46138b
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-06-15 23:24:39 -0400

    update call_expr

--- src/code_generator/codegen/call_expr.hpp
+++ src/code_generator/codegen/call_expr.hpp
@@ -19,15 +19,15 @@ llvm::Value* call_expr::codegen() {
     llvm::Value* ArgValue = arguments[0]->codegen();
     if (!ArgValue) return nullptr;
     llvm::Type* ArgType = ArgValue->getType();
-
+    
     const char* formatStr = nullptr;
     std::vector<llvm::Value*> PrintfArgs;
-
+    
     if (ArgType->isIntegerTy(1)) {  // boolean
       formatStr = "%s\n";
       llvm::Value* TrueStr = Builder->CreateGlobalStringPtr("true", "true_str");
       llvm::Value* FalseStr =
-          Builder->CreateGlobalStringPtr("false", "false_str");
+      Builder->CreateGlobalStringPtr("false", "false_str");
       ArgValue = Builder->CreateSelect(ArgValue, TrueStr, FalseStr);
     } else if (ArgType->isIntegerTy(32)) {  // integer
       formatStr = "%d\n";
@@ -41,24 +41,24 @@ llvm::Value* call_expr::codegen() {
       llvm::errs() << "Unsupported type in print statement\n";
       return nullptr;
     }
-
+    
     // Create format string
     llvm::Value* FormatStr = Builder->CreateGlobalStringPtr(formatStr, "fmt");
     PrintfArgs.push_back(FormatStr);
     PrintfArgs.push_back(ArgValue);
-
+    
     llvm::Function* PrintfF = TheModule->getFunction("printf");
     if (!PrintfF) {
       std::vector<llvm::Type*> printfArgs;
       printfArgs.push_back(Builder->getInt8Ty()->getPointerTo());
       llvm::FunctionType* printfType =
-          llvm::FunctionType::get(Builder->getInt32Ty(), printfArgs, true);
+      llvm::FunctionType::get(Builder->getInt32Ty(), printfArgs, true);
       PrintfF =
-          llvm::Function::Create(printfType, llvm::Function::ExternalLinkage,
-                                 "printf", TheModule.get());
-      PrintfF->setCallingConv(llvm::CallingConv::C);
+      llvm::Function::Create(printfType, llvm::Function::ExternalLinkage,
+        "printf", TheModule.get());
+        PrintfF->setCallingConv(llvm::CallingConv::C);
     }
-
+    
     return Builder->CreateCall(PrintfF, PrintfArgs, "printfcall");
   }
 

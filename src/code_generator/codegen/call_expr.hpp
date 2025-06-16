4f00cf72cc50aeea0afa6878540bc50d16b71e38
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-06-15 21:11:41 -0400

    complete print function

--- src/code_generator/codegen/call_expr.hpp
+++ src/code_generator/codegen/call_expr.hpp
@@ -2,53 +2,70 @@
 #define HULK_CODEGEN_CALLEXPR_HPP 1
 
 #include "../../ast/ast"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Type.h"
 
 namespace hulk {
-    namespace ast {
+namespace ast {
 
-        llvm::Value* call_expr::codegen() {
-            // Special case for print
-            if (callee.lexeme == "print") {
-                if (arguments.size() != 1) {
-                    llvm::errs() << "print function expects exactly one argument\n";
-                    return nullptr;
-                }
+llvm::Value* call_expr::codegen() {
+  // Special case for print
+  if (callee.lexeme == "print") {
+    if (arguments.size() != 1) {
+      llvm::errs() << "print function expects exactly one argument\n";
+      return nullptr;
+    }
 
-                llvm::Value* ArgValue = arguments[0]->codegen();
-                if (!ArgValue)
-                    return nullptr;
+    llvm::Value* ArgValue = arguments[0]->codegen();
+    if (!ArgValue) return nullptr;
+    llvm::Type* ArgType = ArgValue->getType();
 
-                // Create format string
-                llvm::Value* FormatStr = Builder->CreateGlobalStringPtr("%f\n", "fmt", 0, TheModule.get());
+    const char* formatStr = nullptr;
+    std::vector<llvm::Value*> PrintfArgs;
 
-                // Call printf
-                std::vector<llvm::Value*> PrintfArgs;
-                PrintfArgs.push_back(FormatStr);
-                PrintfArgs.push_back(ArgValue);
+    if (ArgType->isIntegerTy(1)) {  // boolean
+      formatStr = "%s\n";
+      llvm::Value* TrueStr = Builder->CreateGlobalStringPtr("true", "true_str");
+      llvm::Value* FalseStr =
+          Builder->CreateGlobalStringPtr("false", "false_str");
+      ArgValue = Builder->CreateSelect(ArgValue, TrueStr, FalseStr);
+    } else if (ArgType->isIntegerTy(32)) {  // integer
+      formatStr = "%d\n";
+    } else if (ArgType->isIntegerTy(64)) {  // long integer
+      formatStr = "%ld\n";
+    } else if (ArgType->isDoubleTy()) {  // float/double
+      formatStr = "%f\n";
+    } else if (ArgType->isPointerTy()) {
+      formatStr = "%s\n";
+    } else {
+      llvm::errs() << "Unsupported type in print statement\n";
+      return nullptr;
+    }
 
-                llvm::Function* PrintfF = TheModule->getFunction("printf");
-                return Builder->CreateCall(PrintfF, PrintfArgs, "printfcall");
-            }
+    // Create format string
+    llvm::Value* FormatStr = Builder->CreateGlobalStringPtr(formatStr, "fmt");
+    PrintfArgs.push_back(FormatStr);
+    PrintfArgs.push_back(ArgValue);
 
-            // Function* CalleeF = TheModule->getFunction(Callee);
-            // if (!CalleeF)
-            //     return LogErrorV("Unknown function referenced");
+    llvm::Function* PrintfF = TheModule->getFunction("printf");
+    if (!PrintfF) {
+      std::vector<llvm::Type*> printfArgs;
+      printfArgs.push_back(Builder->getInt8Ty()->getPointerTo());
+      llvm::FunctionType* printfType =
+          llvm::FunctionType::get(Builder->getInt32Ty(), printfArgs, true);
+      PrintfF =
+          llvm::Function::Create(printfType, llvm::Function::ExternalLinkage,
+                                 "printf", TheModule.get());
+      PrintfF->setCallingConv(llvm::CallingConv::C);
+    }
 
-            // if (CalleeF->arg_size() != Args.size())
-            //     return LogErrorV("Incorrect # arguments passed");
+    return Builder->CreateCall(PrintfF, PrintfArgs, "printfcall");
+  }
 
-            // std::vector<Value*> ArgsV;
-            // for (unsigned i = 0, e = Args.size(); i != e; ++i) {
-            //     ArgsV.push_back(Args[i]->codegen());
-            //     if (!ArgsV.back())
-            //         return nullptr;
-            // }
+  return nullptr;
+}
 
-            // return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
-            return nullptr;
-        }
+}  // namespace ast
+}  // namespace hulk
 
-    } // namespace ast
-} // namespace hulk
-
-#endif // HULK_CODEGEN_CALLEXPR_HPP
+#endif  // HULK_CODEGEN_CALLEXPR_HPP

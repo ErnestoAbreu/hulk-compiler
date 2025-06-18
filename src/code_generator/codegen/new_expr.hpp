fe09b8f40fe9629684b7306001ab9b451b7bad83
ErnestoAbreu <ernestoabreup@gmail.com>
2025-06-18 17:34:21 -0400

    var_expr in progress

--- src/code_generator/codegen/new_expr.hpp
+++ src/code_generator/codegen/new_expr.hpp
@@ -2,52 +2,61 @@
 #define HULK_CODEGEN__EXPR_HPP 1
 
 #include "../../ast/ast"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/Module.h"
 
 namespace hulk {
+    namespace ast {
 
-namespace ast {
+        llvm::Value* new_expr::codegen() {
+            std::string struct_name = type_name.get_lexeme();
+            llvm::StructType* struct_type = llvm::StructType::getTypeByName(*TheContext, struct_name);
 
-llvm::Value* new_expr::codegen() {
-  std::string struct_name = type_name.get_lexeme();
-  llvm::StructType* st = llvm::StructType::getTypeByName(*TheContext, struct_name);
+            llvm::Function* type_ctor = TheModule->getFunction("type." + struct_name + "._ctor");
 
-  auto& DL = TheModule->getDataLayout();
-  uint64_t Size = DL.getTypeAllocSize(st);
-  llvm::Type* IntPtrTy = llvm::Type::getInt64Ty(*TheContext);
+            std::vector<llvm::Value*> args_values;
+            for (const auto& arg : arguments) {
+                llvm::Value* arg_value = arg->codegen();
+                if (!arg_value) {
+                    llvm::errs() << "Error: Failed to codegen argument for new expression\n";
+                    return nullptr;
+                }
+                args_values.push_back(arg_value);
+            }
 
-  llvm::Function* MallocFn = TheModule->getFunction("malloc");
-  if (!MallocFn) {
-    llvm::FunctionType* MallocTy = llvm::FunctionType::get(Builder->getInt8Ty(), {IntPtrTy}, false);
-    MallocFn = llvm::Function::Create(MallocTy, llvm::Function::ExternalLinkage, "malloc", TheModule.get());
-  }
+            llvm::Value* object_ptr = Builder->CreateCall(type_ctor, { args_values }, struct_name + "_ptr");
 
-  llvm::Value* mallocSize = llvm::ConstantInt::get(IntPtrTy, Size);
-  llvm::Value* rawPtr = Builder->CreateCall(MallocFn, {mallocSize}, "rawptr");
+            return object_ptr;
 
-  llvm::PointerType* structPtrTy = st->getPointerTo();
-  llvm::Value* objectPtr = Builder->CreateBitCast(rawPtr, structPtrTy, "objptr");
+            // auto& DL = TheModule->getDataLayout();
+            // uint64_t Size = DL.getTypeAllocSize(struct_type);
+            // llvm::Type* IntPtrTy = llvm::Type::getInt64Ty(*TheContext);
 
-  std::string initName = "init_" + struct_name;
-  if (llvm::Function* initializer = TheModule->getFunction(initName)) {
-    std::vector<llvm::Value*> args;
-    args.push_back(objectPtr);
-    for (auto &arg: arguments) {
-      llvm::Value* v = arg->codegen();
-      args.push_back(v);
-    }
-    Builder->CreateCall(initializer, args, "initcall");
-  }
-  
-  return objectPtr;
-}
+            // llvm::Function* MallocFn = TheModule->getFunction("malloc");
+            // if (!MallocFn) {
+            //     llvm::FunctionType* MallocTy = llvm::FunctionType::get(Builder->getInt8Ty(), { IntPtrTy }, false);
+            //     MallocFn = llvm::Function::Create(MallocTy, llvm::Function::ExternalLinkage, "malloc", TheModule.get());
+            // }
 
-}  // namespace ast
+            // llvm::Value* mallocSize = llvm::ConstantInt::get(IntPtrTy, Size);
+            // llvm::Value* rawPtr = Builder->CreateCall(MallocFn, { mallocSize }, "rawptr");
 
+            // llvm::PointerType* structPtrTy = struct_type->getPointerTo();
+            // llvm::Value* objectPtr = Builder->CreateBitCast(rawPtr, structPtrTy, "objptr");
+
+            // std::string initName = "init_" + struct_name;
+            // if (llvm::Function* initializer = TheModule->getFunction(initName)) {
+            //     std::vector<llvm::Value*> args;
+            //     args.push_back(objectPtr);
+            //     for (auto& arg : arguments) {
+            //         llvm::Value* v = arg->codegen();
+            //         args.push_back(v);
+            //     }
+            //     Builder->CreateCall(initializer, args, "initcall");
+            // }
+
+            // return objectPtr;
+        }
+
+    }  // namespace ast
 }  // namespace hulk
 
 #endif  // HULK_CODEGEN__EXPR_HPP
--- src/code_generator/codegen/var_expr.hpp
+++ src/code_generator/codegen/var_expr.hpp
@@ -7,13 +7,40 @@ namespace hulk {
     namespace ast {
 
         llvm::Value* var_expr::codegen() {
-            llvm::AllocaInst* alloca = NamedValues[name.get_lexeme()];
+            if (object.has_value()) {
+                auto* object_value = (*object)->codegen();
+                if (!object_value) {
+                    llvm::errs() << "Error in var_expr codegen: object value is null: " << name.get_lexeme() << "\n";
+                    return nullptr;
+                }
 
-            if (!alloca)
+                auto* object_type = object_value->getType();
+                if (!object_type->isPointerTy()) {
+                    llvm::errs() << "Error in var_expr codegen: expected pointer type, got " << *object_type << "\n";
+                    return nullptr;
+                }
+
+                auto* object_ptr = Builder->CreateLoad(object_type->getPointerTo(), object_value, "object_ptr");
+
+                if (NamedValues.find(name.get_lexeme()) == NamedValues.end()) {
+                    llvm::errs() << "Error in var_expr codegen: variable '" << name.get_lexeme() << "' not found\n";
+                    return nullptr;
+                }
+
+                //todo
                 return nullptr;
+            }
+            else {
+                llvm::AllocaInst* alloca = NamedValues[name.get_lexeme()];
 
-            auto* allocated_type = alloca->getAllocatedType();
-            return Builder->CreateLoad(allocated_type, alloca, name.get_lexeme());
+                if (!alloca) {
+                    llvm::errs() << "Error in var_expr codegen: " << name.get_lexeme() << "\n";
+                    return nullptr;
+                }
+
+                auto* allocated_type = alloca->getAllocatedType();
+                return Builder->CreateLoad(allocated_type, alloca, name.get_lexeme());
+            }
         }
 
     }  // namespace ast

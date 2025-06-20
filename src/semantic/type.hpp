a0f69df55cc9f68386ea7cc8dddb8176cbda78e7
ErnestoAbreu <ernestoabreup@gmail.com>
2025-06-20 19:13:00 -0400

    refactor type.hpp

--- src/semantic/type.hpp
+++ src/semantic/type.hpp
@@ -9,38 +9,38 @@ namespace hulk {
     namespace semantic {
 
         // Forward declarations
-        struct protocol;
-        struct type;
         struct attribute;
         struct method;
+        struct protocol;
+        struct type;
 
-        using type_ptr = shared_ptr<type>;
         using protocol_ptr = shared_ptr<protocol>;
+        using type_ptr = shared_ptr<type>;
 
         struct attribute {
             string name;
-            string attr_type;
+            type_ptr attr_type;
 
             attribute() = default;
 
-            attribute(const string& attr_name, const string& attr_type = "")
+            attribute(const string& attr_name, const type_ptr& attr_type = nullptr)
                 : name(attr_name), attr_type(attr_type) {
             }
         };
 
         struct method {
             string name;
-            string return_type;
+            type_ptr return_type;
             vector<attribute> params;
 
             method() = default;
 
-            method(const string& method_name, const string& return_type = "")
+            method(const string& method_name, const type_ptr& return_type = nullptr)
                 : name(method_name), return_type(return_type) {
             }
 
             // Define a parameter for the method
-            bool add_param(const string& param_name, const string& param_type = "") {
+            bool add_param(const string& param_name, const type_ptr& param_type = nullptr) {
                 for (const auto& param : params)
                     if (param.name == param_name)
                         return false; // Parameter already exists
@@ -50,23 +50,50 @@ namespace hulk {
             }
 
             attribute& get_param(const string& param_name) {
-                for (auto& param : params) {
-                    if (param.name == param_name) {
+                for (auto& param : params)
+                    if (param.name == param_name)
                         return param;
-                    }
-                }
-                // warn: this is dangerous
-                return *(new attribute());
+
+                return *(new attribute()); // warn: this is dangerous
             }
         };
 
-         struct protocol {
+        struct protocol {
             string name;
             vector<method> methods;
             protocol_ptr parent;
 
             protocol(const string& protocol_name = "")
                 : name(protocol_name) {
+            }
+
+            bool operator==(const protocol& other) const {
+                return name == other.name;
+            }
+
+            bool operator!=(const protocol& other) const {
+                return !(*this == other);
+            }
+
+            bool operator<=(const protocol& other) const {
+                bool conforms = true;
+                for (const auto& method : other.methods) {
+                    if (!has_method(method.name))
+                        return false; // If any method in 'other' is not in 'this', return false
+
+                    auto& this_method = get_method(method.name);
+                    conforms &= this_method.return_type <= method.return_type;
+
+                    if (this_method.params.size() != method.params.size())
+                        return false; // If the number of parameters doesn't match, return false
+
+                    for (size_t i = 0; i < this_method.params.size(); ++i)
+                        conforms &= method.params[i].attr_type <= this_method.params[i].attr_type;
+
+                    if (!conforms)
+                        break;
+                }
+                return conforms;
             }
 
             bool add_method(const method& m) {
@@ -79,29 +106,41 @@ namespace hulk {
             }
 
             bool has_method(const string& method_name) const {
-                for (const auto& method : methods) {
-                    if (method.name == method_name) {
+                for (const auto& method : methods)
+                    if (method.name == method_name)
                         return true;
-                    }
-                }
+
+                if (parent)
+                    return parent->has_method(method_name);
+
                 return false;
             }
 
             method& get_method(const string& method_name) {
-                for (auto& method : methods) {
-                    if (method.name == method_name) {
+                for (auto& method : methods)
+                    if (method.name == method_name)
                         return method;
-                    }
-                }
-                // warn: this is dangerous, make sure to check with has_method first
-                return *(new method());
+
+                if (parent)
+                    return parent->get_method(method_name);
+
+                return *(new method());// warn: this is dangerous, make sure to check with has_method first
+            }
+
+            const method& get_method(const string& method_name) const {
+                for (const auto& method : methods)
+                    if (method.name == method_name)
+                        return method;
+
+                if (parent)
+                    return parent->get_method(method_name);
+
+                return *(new method());// warn: this is dangerous, make sure to check with has_method first
             }
 
             bool add_parent(protocol_ptr parent_protocol) {
-                if (parent) {
-                    // Parent already exists, cannot add another
-                    return false;
-                }
+                if (parent)
+                    return false; // Parent already exists, cannot add another
 
                 parent = parent_protocol;
                 return true;
@@ -116,8 +155,8 @@ namespace hulk {
             type_ptr parent;
             bool is_protocol;
 
-            type(const string& type_name = "")
-                : name(type_name) {
+            type(const string& type_name = "", bool _is_protocol = false)
+                : name(type_name), is_protocol(_is_protocol) {
             }
 
             bool operator==(const type& other) const {
@@ -128,6 +167,26 @@ namespace hulk {
                 return !(*this == other);
             }
 
+            bool operator<=(const protocol& proto) const {
+                bool conforms = true;
+
+                for (const auto& method : proto.methods) {
+                    if (!has_method(method.name))
+                        return false; // If any method in 'proto' is not in 'this', return false
+
+                    auto& this_method = get_method(method.name);
+                    conforms &= this_method.return_type <= method.return_type;
+
+                    if (this_method.params.size() != method.params.size())
+                        return false; // If the number of parameters doesn't match, return false
+
+                    for (size_t i = 0; i < this_method.params.size(); ++i)
+                        conforms &= method.params[i].attr_type <= this_method.params[i].attr_type;
+                }
+
+                return conforms;
+            }
+
             bool operator<=(const type& other) const {
                 if (*this == other) return true;
                 if (*parent <= other)
@@ -136,7 +195,7 @@ namespace hulk {
                 return false;
             }
 
-            bool add_param(const string& param_name, const string& param_type = "") {
+            bool add_param(const string& param_name, const type_ptr& param_type = nullptr) {
                 for (const auto& param : params)
                     if (param.name == param_name)
                         return false; // Parameter already exists
@@ -146,41 +205,42 @@ namespace hulk {
             }
 
             bool add_parent(type_ptr parent_type) {
-                if (parent) {
-                    // Parent already exists, cannot add another
-                    return false;
-                }
+                if (parent)
+                    return false; // Parent already exists
 
                 parent = parent_type;
                 return true;
             }
 
-            bool add_field(const string& field_name, const string& field_type = "") {
+            bool add_field(const string& field_name, const type_ptr& field_type = nullptr) {
                 for (const auto& field : fields)
                     if (field.name == field_name)
-                        return false; // Attribute already exists
+                        return false; // Field already exists
 
                 fields.emplace_back(field_name, field_type);
                 return true;
             }
 
             bool has_field(const string& field_name) const {
-                for (const auto& field : fields) {
-                    if (field.name == field_name) {
+                for (const auto& field : fields)
+                    if (field.name == field_name)
                         return true;
-                    }
-                }
+
+                if (parent)
+                    return parent->has_field(field_name);
+
                 return false;
             }
 
             attribute& get_field(const string& field_name) {
-                for (auto& field : fields) {
-                    if (field.name == field_name) {
+                for (auto& field : fields)
+                    if (field.name == field_name)
                         return field;
-                    }
-                }
-                // warn: this is dangerous, make sure to check with has_field first
-                return *(new attribute());
+
+                if (parent)
+                    return parent->get_field(field_name);
+
+                return *(new attribute()); // warn: this is dangerous, make sure to check with has_field first
             }
 
             bool add_method(const method& m) {
@@ -193,25 +253,39 @@ namespace hulk {
             }
 
             bool has_method(const string& method_name) const {
-                for (const auto& method : methods) {
-                    if (method.name == method_name) {
+                for (const auto& method : methods)
+                    if (method.name == method_name)
                         return true;
-                    }
-                }
+
+                if (parent)
+                    return parent->has_method(method_name);
+
                 return false;
             }
 
             method& get_method(const string& method_name) {
-                for (auto& method : methods) {
-                    if (method.name == method_name) {
+                for (auto& method : methods)
+                    if (method.name == method_name)
                         return method;
-                    }
-                }
-                // warn: this is dangerous, make sure to check with has_method first
-                return *(new method());
+
+                if (parent)
+                    return parent->get_method(method_name);
+
+                return *(new method()); // warn: this is dangerous, make sure to check with has_method first
             }
 
+            const method& get_method(const string& method_name) const {
+                for (const auto& method : methods)
+                    if (method.name == method_name)
+                        return method;
+
+                if (parent)
+                    return parent->get_method(method_name);
+
+                return *(new method()); // warn: this is dangerous, make sure to check with has_method first
+            }
         };
+
     }
 }
 

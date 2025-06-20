23d2351286be8f50f91aca225c05d669c8e71344
ErnestoAbreu <ernestoabreup@gmail.com>
2025-06-20 16:57:43 -0400

    refactor internal_error

--- src/internal/internal_error.hpp
+++ src/internal/internal_error.hpp
@@ -7,45 +7,34 @@
 #include "../lexer/tokens"
 
 namespace hulk {
+  namespace internal {
 
-namespace internal {
+    static bool error_found = false;
 
-  static bool error_found = false;
-  static bool runtime_error_found = false;
-
-  static void print(const unsigned int line, const unsigned int column, const std::string where, const std::string message) {
-    std::cerr << "[" << line << ", " << column << "] Parse error" << where << ": " << message << "\n";
-    error_found = true;
-  }
-
-  static void error(const unsigned int line, const unsigned int column, const std::string message) {
-    print(line, column, "", message);
-  }
-
-  static void error(const lexer::token &token, const std::string message) {
-    if (token.get_type() == lexer::token_type::END_OF_FILE) {
-      print(token.get_location().first, token.get_location().second, " at end", message);
-    } else {
-      print(token.get_location().first, token.get_location().second, " at '" + std::string(token.get_lexeme()) + "'", message);
+    static void print(const unsigned int line, const unsigned int column, const std::string where, const std::string message) {
+      std::cerr << "[" << line << ", " << column << "] Parse error" << where << ": " << message << "\n";
+      error_found = true;
     }
-  }
-  
-  struct runtime_error final : std::runtime_error {
-    lexer::token token;
-    explicit runtime_error(const lexer::token &_token, const std::string &_message) : std::runtime_error(_message), token(_token) {}
-  };
 
-  static void rte(const runtime_error &error) {
-    std::cerr << error.what() << "\n[line " << error.token.get_location().first << "]";
-    runtime_error_found = true;
-  }
+    static void error(const std::string message, const std::string component = "") {
+      std::cerr << "Internal Error: " << component << " " << message << "\n";
+      error_found = true;
+    }
 
-  static void semantic_error(const unsigned int line, unsigned int column, const std::string &message) {
-    std::cerr << "[line " << line << ", column " << column << "] Semantic Error:\n\t" << message << "\n";
-    error_found = true;
-  }
+    static void error(const unsigned int line, const unsigned int column, const std::string message) {
+      print(line, column, "", message);
+    }
 
-} // namespace internal
+    static void error(const lexer::token& token, const std::string message) {
+      if (token.get_type() == lexer::token_type::END_OF_FILE) {
+        print(token.get_location().first, token.get_location().second, " at end", message);
+      }
+      else {
+        print(token.get_location().first, token.get_location().second, " at '" + std::string(token.get_lexeme()) + "'", message);
+      }
+    }
+
+  } // namespace internal
 
 } // namespace hulk
 

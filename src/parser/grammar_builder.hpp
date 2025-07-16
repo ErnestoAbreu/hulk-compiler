29fef0bc6ef0fd673d84f53ca1867365c1a328ad
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-07-16 18:22:13 -0400

    fix hulk grammar

--- src/parser/grammar_builder.hpp
+++ src/parser/grammar_builder.hpp
@@ -33,6 +33,10 @@ struct grammar_builder {
   void set_start(string new_start) { start = new_start; }
 
   void add_production(string left, vector<symbol> right) {
+    if (right.empty()) {
+      add_epsilon_production(left);
+      return;
+    }
     grammar[left].emplace_back();
     for (auto &p : right)
       grammar[left].back().push_back(p.repr);
@@ -44,197 +48,175 @@ struct grammar_builder {
 };
 
 struct hulk_grammar : public grammar_builder {
-  hulk_grammar() : grammar_builder("program") { build_hulk_grammar(); }
+  hulk_grammar() : grammar_builder("program") { build_grammar(); }
 
  private:
-  void build_hulk_grammar() {
-    // Programa principal
+  void build_grammar() {
     add_production("program", {"declaration_list"});
-    add_production("declaration_list",
-                   {"declaration", "declaration_list_tail"});
-    add_production("declaration_list_tail",
-                   {SEMICOLON, "declaration", "declaration_list_tail"});
-    add_epsilon_production("declaration_list_tail");
 
-    // Declaraciones
+    add_production("declaration_list", {"declaration", "sep_semicolon", "declaration_list"});
+    add_epsilon_production("declaration_list");
+
+    add_production("sep_semicolon", {SEMICOLON});
+    add_production("sep_semicolon", {D_SEMICOLON});
+
     add_production("declaration", {"function_decl"});
     add_production("declaration", {"type_decl"});
     add_production("declaration", {"protocol_decl"});
     add_production("declaration", {"stmt"});
-
-    // Funciones
-    add_production("function_decl",
-                   {KW_FUNCTION, IDENTIFIER, LPAREN, "params", RPAREN,
-                    "type_annotation", "function_body"});
-    add_production("params", {"param_list"});
-    add_epsilon_production("params");
+    
+    add_production("function_decl", {KW_FUNCTION, IDENTIFIER, LPAREN, "opt_param_list", RPAREN, "opt_type_annotation", "function_body"});
+    
+    add_production("opt_param_list", {"param_list"});
+    add_epsilon_production("opt_param_list");
+    
     add_production("param_list", {"param", "param_list_tail"});
     add_production("param_list_tail", {COMMA, "param", "param_list_tail"});
+    add_production("param", {IDENTIFIER, "opt_type_annotation"});
     add_epsilon_production("param_list_tail");
-    add_production("param", {IDENTIFIER, "type_annotation"});
 
-    // Tipos
-    add_production("type_decl", {KW_TYPE, IDENTIFIER, "inheritance", LBRACE,
-                                 "type_body", RBRACE});
+    add_production("function_body", {ARROW, "expression"});
+    add_production("function_body", {LBRACE, "expression_list", RBRACE});
+
+    add_production("type_decl", {KW_TYPE, IDENTIFIER, "type_params", "inheritance", LBRACE, "type_body", RBRACE});
+    add_production("type_params", {LPAREN, "args", RPAREN});
+    add_epsilon_production("type_params");
+    
     add_production("inheritance", {KW_INHERITS, IDENTIFIER, "parent_args"});
     add_epsilon_production("inheritance");
     add_production("parent_args", {LPAREN, "args", RPAREN});
     add_epsilon_production("parent_args");
-    add_production("type_body", {"member", "type_body_tail"});
+
+    add_production("type_body", {"member", "type_body"});
     add_epsilon_production("type_body");
-    add_production("type_body_tail", {SEMICOLON, "member", "type_body_tail"});
-    add_epsilon_production("type_body_tail");
-    add_production("member", {IDENTIFIER, "type_annotation", "initializer"});
-    add_production("member", {"function_decl"});
 
-    // Protocolos
-    add_production("protocol_decl",
-                   {KW_PROTOCOL, IDENTIFIER, "protocol_inheritance", LBRACE,
-                    "protocol_body", RBRACE});
+    add_production("member", {IDENTIFIER, "member_tail"});
+    add_production("member_tail", {"opt_type_annotation", "opt_initializer", SEMICOLON});
+    add_production("opt_initializer", {"initializer"});
+    add_epsilon_production("opt_initializer");
+
+    add_production("member_tail", {LPAREN, "args", RPAREN, "opt_type_annotation", "function_body", "sep_semicolon"});
+
+    add_production("protocol_decl", {KW_PROTOCOL, IDENTIFIER, "protocol_inheritance", LBRACE, "protocol_body", RBRACE});
+
     add_production("protocol_inheritance", {KW_EXTENDS, IDENTIFIER});
-    add_epsilon_production("protocol_inheritance");
     add_production("protocol_body", {"protocol_method", "protocol_body_tail"});
-    add_epsilon_production("protocol_body");
-    add_production("protocol_body_tail",
-                   {SEMICOLON, "protocol_method", "protocol_body_tail"});
-    add_epsilon_production("protocol_body_tail");
-    add_production("protocol_method",
-                   {IDENTIFIER, LPAREN, "params", RPAREN, "type_annotation"});
-
-    // Anotaciones de tipo
+    add_production("protocol_body_tail", {SEMICOLON, "protocol_method", "protocol_body_tail"});
+    add_production("protocol_method", {IDENTIFIER, LPAREN, "method_params", RPAREN, "type_annotation", SEMICOLON});
     add_production("type_annotation", {COLON, IDENTIFIER});
-    add_epsilon_production("type_annotation");
+    add_epsilon_production("protocol_body_tail");
+    add_epsilon_production("protocol_body");
+    
+    add_production("method_params", {"m_param_list"});
+    add_epsilon_production("method_params");
 
-    // Cuerpo de función
-    add_production("function_body", {ARROW, "expr"});
-    add_production("function_body", {LBRACE, "stmt_list", RBRACE});
+    add_production("m_param_list", {"m_param", "m_param_list_tail"});
+    add_production("m_param_list_tail", {COMMA, "m_param", "m_param_list_tail"});
+    add_production("m_param", {IDENTIFIER, "type_annotation"});
+    add_epsilon_production("m_param_list_tail");
 
-    // Inicializadores
-    add_production("initializer", {OP_ASSIGN, "expr"});
-    add_epsilon_production("initializer");
+    add_production("stmt", {"expression_statement"});
+    add_production("expression_statement", {"expression"});
+    
+    add_production("expression", {"let_expr"});
+    add_production("expression", {"if_expr"});
+    add_production("expression", {"while_expr"});
+    add_production("expression", {"block_expr"});
+    add_production("expression", {"for_expr"});
+    add_production("expression", {"assignment"});
 
-    // Declaraciones
-    add_production("stmt", {"let_stmt"});
-    add_production("stmt", {"if_stmt"});
-    add_production("stmt", {"while_stmt"});
-    add_production("stmt", {"for_stmt"});
-    add_production("stmt", {"block"});
-    add_production("stmt", {"expr_stmt"});
+    add_production("opt_expression", {"expression"});
+    add_epsilon_production("opt_expression");
 
-    // Let
-    add_production("let_stmt", {KW_LET, "binding_list", KW_IN, "expr"});
+    add_production("expression_list", {"opt_expression", "expression_list_tail"});
+    add_production("expression_list_tail", {SEMICOLON, "opt_expression", "expression_list_tail"});
+    add_epsilon_production("expression_list_tail");
+    
+    add_production("block_expr", {LBRACE, "expression_list", RBRACE});
+
+    add_production("let_expr", {KW_LET, "binding_list", KW_IN, "expression"});
+
+    add_production("binding", {IDENTIFIER, "opt_type_annotation", "initializer"});
+
     add_production("binding_list", {"binding", "binding_list_tail"});
-    add_production("binding_list_tail",
-                   {COMMA, "binding", "binding_list_tail"});
+    add_production("binding_list_tail", {COMMA, "binding", "binding_list_tail"});
     add_epsilon_production("binding_list_tail");
-    add_production("binding", {IDENTIFIER, "type_annotation", "initializer"});
 
-    // If
-    add_production("if_stmt", {KW_IF, LPAREN, "expr", RPAREN, "expr",
-                               "elif_list", "else_clause"});
-    add_production("elif_list",
-                   {KW_ELIF, LPAREN, "expr", RPAREN, "expr", "elif_list"});
+    add_production("opt_type_annotation", {COLON, IDENTIFIER});
+    add_epsilon_production("opt_type_annotation");
+  
+    add_production("initializer", {OP_ASSIGN, "expression"});
+
+    add_production("if_expr", {KW_IF, LPAREN, "expression", RPAREN, "expression", "elif_list", KW_ELSE, "expression"});
+    add_production("elif_list", {KW_ELIF, LPAREN, "expression", RPAREN, "expression", "elif_list"});
     add_epsilon_production("elif_list");
-    add_production("else_clause", {KW_ELSE, "expr"});
-    add_epsilon_production("else_clause");
+    
+    add_production("while_expr", {KW_WHILE, LPAREN, "expression", RPAREN, "expression"});
+    add_production("for_expr", {KW_FOR, LPAREN, IDENTIFIER, "opt_type_annotation", KW_IN, "expression", RPAREN, "expression"});
 
-    // While
-    add_production("while_stmt", {KW_WHILE, LPAREN, "expr", RPAREN, "expr"});
-
-    // For
-    add_production("for_stmt",
-                   {KW_FOR, LPAREN, "binding", KW_IN, "expr", RPAREN, "expr"});
-
-    // Bloques
-    add_production("block", {LBRACE, "stmt_list", RBRACE});
-    add_production("stmt_list", {"stmt", "stmt_list_tail"});
-    add_production("stmt_list_tail", {SEMICOLON, "stmt", "stmt_list_tail"});
-    add_epsilon_production("stmt_list_tail");
-
-    // Expresiones
-    add_production("expr_stmt", {"expr"});
-    add_production("expr", {"assignment"});
-
-    // Asignaciones
-    add_production("assignment", {"logic_or", "assignment_tail"});
-    add_production("assignment_tail", {OP_DESTRUCT_ASSIGN, "assignment"});
+    add_production("assignment", {"or_expr", "assignment_tail"});
+    add_production("assignment_tail", {OP_DESTRUCT_ASSIGN, "or_expr", "assignment_tail"});
     add_epsilon_production("assignment_tail");
 
-    // Lógica
-    add_production("logic_or", {"logic_and", "logic_or_tail"});
-    add_production("logic_or_tail", {OP_OR, "logic_and", "logic_or_tail"});
-    add_epsilon_production("logic_or_tail");
-    add_production("logic_and", {"equality", "logic_and_tail"});
-    add_production("logic_and_tail", {OP_AND, "equality", "logic_and_tail"});
-    add_epsilon_production("logic_and_tail");
+    add_production("or_expr", {"and_expr", "or_expr_tail"});
+    add_production("or_expr_tail", {OP_OR, "and_expr", "or_expr_tail"});    
+    add_epsilon_production("or_expr_tail");
 
-    // Igualdad
+    add_production("and_expr", {"equality", "and_expr_tail"});
+    add_production("and_expr_tail", {OP_AND, "equality", "and_expr_tail"});
+    add_epsilon_production("and_expr_tail");
+
     add_production("equality", {"comparison", "equality_tail"});
-    add_production("equality_tail",
-                   {"equality_op", "comparison", "equality_tail"});
+    add_production("equality_tail", {OP_NOT_EQUAL, "comparison", "equality_tail"});
+    add_production("equality_tail", {OP_EQUAL, "comparison", "equality_tail"});
     add_epsilon_production("equality_tail");
-    add_production("equality_op", {OP_EQUAL});
-    add_production("equality_op", {OP_NOT_EQUAL});
 
-    // Comparación
     add_production("comparison", {"term", "comparison_tail"});
-    add_production("comparison_tail",
-                   {"comparison_op", "term", "comparison_tail"});
+    add_production("comparison_tail", {OP_GREATER, "term", "comparison_tail"});
+    add_production("comparison_tail", {OP_GREATER_EQ, "term", "comparison_tail"});
+    add_production("comparison_tail", {OP_LESS, "term", "comparison_tail"});
+    add_production("comparison_tail", {OP_LESS_EQ, "term", "comparison_tail"});
     add_epsilon_production("comparison_tail");
-    add_production("comparison_op", {OP_LESS});
-    add_production("comparison_op", {OP_LESS_EQ});
-    add_production("comparison_op", {OP_GREATER});
-    add_production("comparison_op", {OP_GREATER_EQ});
 
-    // Términos
     add_production("term", {"factor", "term_tail"});
-    add_production("term_tail", {"add_op", "factor", "term_tail"});
+    add_production("term_tail", {OP_MINUS, "factor", "term_tail"});
+    add_production("term_tail", {OP_PLUS, "factor", "term_tail"});
     add_epsilon_production("term_tail");
-    add_production("add_op", {OP_PLUS});
-    add_production("add_op", {OP_MINUS});
 
-    // Factores
     add_production("factor", {"unary", "factor_tail"});
-    add_production("factor_tail", {"mult_op", "unary", "factor_tail"});
+    add_production("factor_tail", {OP_DIVIDE, "unary", "factor_tail"});
+    add_production("factor_tail", {OP_MODULE, "unary", "factor_tail"});
+    add_production("factor_tail", {OP_MULTIPLY, "unary", "factor_tail"});
+    add_production("factor_tail", {OP_EXPONENT, "unary", "factor_tail"});
+    add_production("factor_tail", {OP_CONCAT, "unary", "factor_tail"});
+    add_production("factor_tail", {OP_DOBLE_CONCAT, "unary", "factor_tail"});
     add_epsilon_production("factor_tail");
-    add_production("mult_op", {OP_MULTIPLY});
-    add_production("mult_op", {OP_DIVIDE});
-    add_production("mult_op", {OP_MODULE});
-    add_production("mult_op", {OP_EXPONENT});
-    add_production("mult_op", {OP_CONCAT});
-    add_production("mult_op", {OP_DOBLE_CONCAT});
 
-    // Unarios
-    add_production("unary", {"unary_op", "unary"});
-    add_production("unary", {"call"});
-    add_production("unary_op", {OP_NOT});
-    add_production("unary_op", {OP_MINUS});
+    add_production("unary", {OP_NOT, "unary"});
+    add_production("unary", {OP_MINUS, "unary"});
+    add_production("unary", {"var_call"});
 
-    // Llamadas
-    add_production("call", {"primary", "call_tail"});
-    add_production("call_tail", {DOT, IDENTIFIER, "call_suffix"});
-    add_epsilon_production("call_tail");
-    add_production("call_suffix", {LPAREN, "args", RPAREN});
-    add_production("call_suffix", {});  // Acceso a propiedad
+    add_production("var_call", {"primary", "var_call_tail"});
+    add_production("var_call_tail", {DOT, IDENTIFIER, "finish_call", "var_call_tail"});
+    add_epsilon_production("var_call_tail");
 
-    // Argumentos
+    add_production("finish_call", {LPAREN, "args", RPAREN});
+    add_epsilon_production("finish_call");
+
     add_production("args", {"expr_list"});
-    add_epsilon_production("args");
-    add_production("expr_list", {"expr", "expr_list_tail"});
-    add_production("expr_list_tail", {COMMA, "expr", "expr_list_tail"});
+    add_epsilon_production("expr_list");
+    add_production("expr_list", {"expression", "expr_list_tail"});
+    add_production("expr_list_tail", {COMMA, "expression", "expr_list_tail"});
     add_epsilon_production("expr_list_tail");
 
-    // Primarios
-    add_production("primary", {TRUE});
     add_production("primary", {FALSE});
+    add_production("primary", {TRUE});
     add_production("primary", {T_NULL});
     add_production("primary", {NUMBER});
     add_production("primary", {STRING});
-    add_production("primary", {IDENTIFIER, "identifier_tail"});
-    add_production("primary", {LPAREN, "expr", RPAREN});
+    add_production("primary", {IDENTIFIER, "finish_call"});
+    add_production("primary", {LPAREN, "expression", RPAREN});
     add_production("primary", {KW_NEW, IDENTIFIER, LPAREN, "args", RPAREN});
-    add_production("identifier_tail", {LPAREN, "args", RPAREN});
-    add_epsilon_production("identifier_tail");
   }
 };
 
